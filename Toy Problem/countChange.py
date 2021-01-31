ac8d4ce51f6f2d08da892a581d931b8be3de19dc
tanmaytoshniwal <tanmaymaheshwari040@gmail.com>
2021-02-01 01:11:24 +0530

    add files

--- Toy Problem/countChange.py
+++ Toy Problem/countChange.py
@@ -1,5 +1,5 @@
-target = 200 #4
-coins = [1,2,5,10,20,50,100,200] #[1,2,3]
+target = 100 #4
+coins = [10,20,50] #[1,2,3]
 ways = [1]+[0]*target
 for coin in coins:
     for i in range(coin,target+1):
